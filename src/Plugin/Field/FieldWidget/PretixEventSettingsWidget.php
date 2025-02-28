7aadcdfaf82a2f72c5c246f2150a69db75347823
Mikkel Ricky <rimi@aarhus.dk>
2025-02-28 01:01:18 +0100

    Cleaned up

--- src/Plugin/Field/FieldWidget/PretixEventSettingsWidget.php
+++ src/Plugin/Field/FieldWidget/PretixEventSettingsWidget.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\itk_pretix\Plugin\Field\FieldWidget;
 
-use Drupal\Core\Database\Exception\EventException;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\WidgetBase;
